5736b389f737635fb94444bb69363329edc121dd
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    chiron: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -33,6 +33,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+    BuildDesc="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys" \
+    BuildFingerprint=Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
